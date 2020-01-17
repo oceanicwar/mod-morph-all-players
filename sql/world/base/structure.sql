12e491b6f3eb81de55e92f2a253989cbe44419e9
Barbz <BarbzYHOOL@users.noreply.github.com>
2020-01-17 16:16:42 +0100

    fix(sql): trinity_string to acore_string

--- sql/world/base/structure.sql
+++ sql/world/base/structure.sql
@@ -1,4 +1,4 @@
 -- COMMANDS
 INSERT INTO `command` VALUES ('allmorph', 3, 'Syntax: .allmorph #displayid \r\n\r\nChange players model id to #displayid.');
 INSERT INTO `command` VALUES ('alldemorph', 3, 'Syntax: .alldemorph #displayid \r\n\r\nDemorph all players.');
-INSERT INTO `trinity_string` VALUES (40000, 'Reloaded morph_all_command', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `acore_string` VALUES (40000, 'Reloaded morph_all_command', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
